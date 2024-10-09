12cee40ea34c03018c661558719f7060b9969968
Diego Assunção Leite <diegoleitecba1986@gmail.com>
2024-10-09 13:25:18 -0400

    subindo

--- index.php
+++ index.php
@@ -12,10 +12,16 @@ if ($sql->rowCount() > 0) {
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Listar Usuários</title>
 </head>
+<style>
+    body {
+        background-color: #f0f0f0; /* Cor de fundo cinza claro */
+    }
+</style>
 <body>
     <div class="container mt-5">
         <h1>Lista de Usuários</h1>
